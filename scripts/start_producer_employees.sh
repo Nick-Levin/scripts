7a9c79c2c9b5f443b642f024c874c1eec8f9975b
Nick Levin <35115692+Nick-Levin@users.noreply.github.com>
2019-12-22 12:50:58 +0200

    Add files via upload

---
+++ scripts/start_producer_employees.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export THEM=employees
+export TOPIC=${THEM}Topic
+
+export PARAMS="--logging.file=log/${TOPIC}-log.log --kafka.producer.topic=$TOPIC --kafka.botstrap.servers=$KAFKA_SERVERS"
+
+java -jar AttunitySimulatorC123-1.0.0-SNAPSHOT.jar $PARAMS > ${THEM}-log.log 2>&1 &
